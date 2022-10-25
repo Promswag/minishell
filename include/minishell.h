525678f72116138badd933dd50fdbb98105a4e84
Guillaume Baumgartner <gbaumgar@k0r2p13.42mulhouse.fr>
2022-10-25 16:16:44 +0200

    coucou

--- include/minishell.h
+++ include/minishell.h
@@ -6,11 +6,22 @@
 /*   By: gbaumgar <gbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 12:10:43 by gbaumgar          #+#    #+#             */
-/*   Updated: 2022/10/21 12:11:18 by gbaumgar         ###   ########.fr       */
+/*   Updated: 2022/10/25 15:18:10 by gbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include "../libft/include/libft.h"
+
+typedef struct s_command
+{
+	char				*command;
+	char				**args;
+	char				*stdout;
+	int					fd;
+	struct s_command	*next;
+}	t_command;
 
 #endif
