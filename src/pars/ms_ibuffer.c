5da154d9a161caa62b28aebd125cd54ec7a7fca1
aho <antoine.ho@outlook.com>
2022-11-21 14:20:16 +0100

    pars5.0, ajout des heredoc quoted, changement des fd in/out, creation de la list fdlist; todo = syntax error

--- src/pars/ms_ibuffer.c
+++ src/pars/ms_ibuffer.c
@@ -63,7 +63,7 @@ int	ms_ibuffer(t_tmp **tmp, int index, int field_buff, const char *str)
 	while (str[index] == 60 || str[index] == ' ')
 		index++;
 	quote.i = index;
-	if (str[index] == 34 || str[index] == 39)
+	if ((str[index] == 34 || str[index] == 39) && field_buff == 3)
 		field_buff = 12;
 	while (str[index])
 	{
