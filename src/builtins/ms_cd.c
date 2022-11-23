ca372a4d7aa05c8033f7b0dd60c3927dc86bd74b
Guillaume Baumgartner <gbaumgar@k0r2p13.42mulhouse.fr>
2022-11-23 11:27:38 +0100

    fix cd, pwd

--- src/builtins/ms_cd.c
+++ src/builtins/ms_cd.c
@@ -6,73 +6,69 @@
 /*   By: gbaumgar <gbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 13:00:00 by gbaumgar          #+#    #+#             */
-/*   Updated: 2022/11/22 17:16:41 by gbaumgar         ###   ########.fr       */
+/*   Updated: 2022/11/23 11:19:03 by gbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <sys/types.h>
+#include <dirent.h>
 
-static int	ms_cd_error(void)
+static void	ms_cd_error(void)
 {
 	write(STDERR_FILENO, SHELL_NAME, ft_strlen(SHELL_NAME));
 	write(STDERR_FILENO, ": cd: HOME not set\n", 19);
 	g_exit_code = 1;
-	return (1);
 }
 
-void	ms_cd_home(char *buf, t_command *cmd, char ***env)
+void	mc_cd_target(char *target, char *oldpwd, char *pwd, char ***env)
 {
-	char	*pwd;
-	char	*oldpwd;
-	char	*home;
+	char	buf[1000];
 
-	home = ms_export_get_value("HOME", *env);
-	if (!home)
+	if (target)
 	{
-		ms_cd_error();
-		return ;
+		if (chdir(target))
+			ms_error_s("cd", target);
+		else
+		{
+			if (getcwd(buf, 1000) == NULL)
+				ms_error("cd");
+			else
+			{
+				pwd = ft_strjoin("PWD=", buf);
+				ms_export(&(t_command){0, \
+					(char *[]){"export", oldpwd, pwd, 0}, 0, 0}, env);
+				g_exit_code = 0;
+			}
+		}
 	}
 	else
-		pwd = ft_strjoin("PWD=", home);
-	oldpwd = ft_strjoin("OLDPWD=", buf);
-	pwd = ft_strjoin("PWD=", home);
-	if (chdir(home))
-		ms_error_s(cmd->args[0], home);
-	else
-		ms_export(&(t_command){0, (char *[]){oldpwd, pwd, 0}, 0, 0}, env);
-	free(oldpwd);
-	free(pwd);
-	free(home);
-	g_exit_code = 0;
-}
-
-void	ms_cd_default(char *buf, t_command *cmd, char ***env)
-{
-	char	*pwd;
-	char	*oldpwd;
-
-	oldpwd = ft_strjoin("OLDPWD=", buf);
-	pwd = ft_strjoin("PWD=", cmd->args[1]);
-	if (chdir(cmd->args[1]))
-		ms_error_s(cmd->args[0], cmd->args[1]);
-	else
-		ms_export(&(t_command){0, (char *[]){oldpwd, pwd, 0}, 0, 0}, env);
-	free(oldpwd);
-	free(pwd);
-	g_exit_code = 0;
+		ms_cd_error();
 }
 
 void	ms_cd(t_command *cmd, char ***env)
 {
 	char	buf[1000];
+	char	*oldpwd;
+	char	*pwd;
+	char	*target;
 
 	if (getcwd(buf, 1000) == NULL)
 	{
 		ms_error("cd");
 		return ;
 	}
+	oldpwd = ft_strjoin("OLDPWD=", buf);
+	pwd = NULL;
 	if (cmd->args[1])
-		ms_cd_default(buf, cmd, env);
+		target = cmd->args[1];
 	else
-		ms_cd_home(buf, cmd, env);
+		target = ms_export_get_value("HOME", *env);
+	mc_cd_target(target, oldpwd, pwd, env);
+	if (oldpwd)
+		free(oldpwd);
+	if (pwd)
+		free(pwd);
+	if (!cmd->args[1] && target)
+		free(target);
 }
--- src/builtins/ms_pwd.c
+++ src/builtins/ms_pwd.c
@@ -6,7 +6,7 @@
 /*   By: gbaumgar <gbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 11:27:29 by gbaumgar          #+#    #+#             */
-/*   Updated: 2022/11/22 13:02:43 by gbaumgar         ###   ########.fr       */
+/*   Updated: 2022/11/23 11:23:11 by gbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ void	ms_pwd(t_command *cmd, char ***env)
 
 	(void)cmd;
 	(void)env;
-	getcwd(path, 100);
-	if (errno)
-		perror("");
+	if (getcwd(path, 1000) == NULL)
+	{
+		ms_error("cd");
+		return ;
+	}
 	write(STDOUT_FILENO, path, ft_strlen(path));
 	write(STDOUT_FILENO, "\n", 1);
 	g_exit_code = 0;
